adcda4a5b0ddc5e6f9b42c628a0581eb4471fe6f
Éverson Vieira de Lima <eversonvieiradelima@gmail.com>
2023-09-05 00:28:41 -0300

    Edição Accordion

--- src/Components/Accordion/Duvidas.jsx
+++ src/Components/Accordion/Duvidas.jsx
@@ -8,8 +8,8 @@ export default function Duvidas() {
     <div className="containerDuvidas">
       <h2 className="titleDuvidas">Dúvidas Frequentes</h2>
       <Accordion className="accordion">
-        <Accordion.Item eventKey="0">
-          <Accordion.Header>
+        <Accordion.Item eventKey="0" className="accordionItem">
+          <Accordion.Header className="accordionHeader">
             Quero cancelar a compra do meu pacote. O que preciso saber?
           </Accordion.Header>
           <Accordion.Body>
@@ -24,7 +24,7 @@ export default function Duvidas() {
             ...
           </Accordion.Body>
         </Accordion.Item>
-        <Accordion.Item eventKey="1">
+        <Accordion.Item eventKey="1" className="accordionItem">
           <Accordion.Header>
             Preciso imprimir minha passagem aérea para apresentar no embarque?
           </Accordion.Header>
@@ -34,24 +34,24 @@ export default function Duvidas() {
             <p>O seu cartão de embarque digital com os dados de viagem também fica disponível no site da companhia aérea responsável pelo voo, aplicativo ou totens de autoatendimento.</p>
           </Accordion.Body>
         </Accordion.Item>
-        <Accordion.Item eventKey="2">
+        <Accordion.Item eventKey="2" className="accordionItem">
           <Accordion.Header>
             Consigo realizar uma remarcação do meu voo ou da minha hospedagem?
           </Accordion.Header>
           <Accordion.Body>
             <p>As solicitações de remarcação dependem da disponibilidade e das regras dos fornecedores, levando em consideração o produto adquirido, devendo serem realizadas com a antecedência mínima de 60 dias em relação à data de embarque.</p>
 
-            <p>E para verificarmos se a sua solicitação é elegível para remarcação, precisaremos analisar as características da sua compra. Por isso, preencha o nosso Formulário de ajuda (<a href="/ a">clique aqui</a>), informando o código do pedido ou localizador, além do e-mail ou CPF do pagante.</p>
+            <p>E para verificarmos se a sua solicitação é elegível para remarcação, precisaremos analisar as características da sua compra. Por isso, preencha o nosso Formulário de ajuda (<a href="/" className="links">clique aqui</a>), informando o código do pedido ou localizador, além do e-mail ou CPF do pagante.</p>
 
             <p>Após isso, solicitamos que aguarde a resposta no e-mail cadastrado, que será enviada o mais breve possível.</p>
           </Accordion.Body>
         </Accordion.Item>
-        <Accordion.Item eventKey="3">
+        <Accordion.Item eventKey="3" className="accordionItem">
           <Accordion.Header>
             Como solicito a nota fiscal da minha reserva de hotel?
           </Accordion.Header>
           <Accordion.Body>
-            <p>Você solicita a Nota Fiscal da sua hospedagem por meio do nosso Formulário de Ajuda (<a href="/">clique aqui</a>), informando o código do pedido ou localizador, além do E-mail ou CPF do pagante. </p>
+            <p>Você solicita a Nota Fiscal da sua hospedagem por meio do nosso Formulário de Ajuda (<a href="/" className="links">clique aqui</a>), informando o código do pedido ou localizador, além do E-mail ou CPF do pagante. </p>
 
             <p>Após recebermos o seu contato, pedimos que aguarde a resposta no e-mail cadastrado, que será enviada o mais breve possível.</p>
           </Accordion.Body>
