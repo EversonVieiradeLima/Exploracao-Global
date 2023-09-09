08f73a61ec9aff658994d6175d991b46257b209a
Éverson Vieira de Lima <eversonvieiradelima@gmail.com>
2023-09-09 09:29:52 -0300

    Alteração de comportamento no componente NavbarButtons

--- src/Components/NavbarButtons/NavbarButtons.jsx
+++ src/Components/NavbarButtons/NavbarButtons.jsx
@@ -1,52 +1,59 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 
 import "./style.css";
+import { PiSuitcaseLight } from "react-icons/pi";
 import { Building } from "react-bootstrap-icons";
 import { TfiMapAlt } from "react-icons/tfi";
 import { LuPlane } from "react-icons/lu";
 import { IoIosPeople } from "react-icons/io";
 
 export default function NavbarButtons() {
+  const location = useLocation();
+
    return (
       <div className="boxButtonOption">
-          {/* <Button variant="outline-primary" size="lg" className="buttonOption on">Home</Button> */}
           <div className="boxButton">
-            <Button
-              variant="outline-primary"
-              size="lg"
-              className="buttonOption"
-            >
+            <NavLink to="/" currentPath={location.pathname}>
+              <PiSuitcaseLight className="iconButton" /> Pacotes
+            </NavLink>
+          </div>
+          <div className="boxButton">
+            <NavLink to="/hospedagens" currentPath={location.pathname}>
               <Building className="iconButton" /> Hospedagens
-            </Button>
+            </NavLink>
           </div>
           <div className="boxButton">
-            <Button
-              variant="outline-primary"
-              size="lg"
-              className="buttonOption"
-            >
+            <NavLink to="/destinos-turisticos" currentPath={location.pathname}>
               <TfiMapAlt className="iconButton" /> Destinos Turísticos
-            </Button>
+            </NavLink>
           </div>
           <div className="boxButton">
-            <Button
-              variant="outline-primary"
-              size="lg"
-              className="buttonOption"
-            >
+            <NavLink to="/passagens-aereas" currentPath={location.pathname}>
               <LuPlane className="iconButton" /> Passagens Aéreas
-            </Button>
+            </NavLink>
           </div>
           <div className="boxButton">
-            <Button
-              variant="outline-primary"
-              size="lg"
-              className="buttonOption"
-            >
+            <NavLink to="/quem-somos" currentPath={location.pathname}>
               <IoIosPeople className="iconButton" /> Quem Somos
-            </Button>
+            </NavLink>
           </div>
         </div>
    );
+}
+
+function NavLink({ to, currentPath, children }) {
+  const isActive = to === currentPath;
+
+  return (
+    <Button
+      as={Link}
+      to={to}
+      size="lg"
+      className={`buttonOption ${isActive ? "on" : ""}`}
+    >
+      {children}
+    </Button>
+  );
 }
