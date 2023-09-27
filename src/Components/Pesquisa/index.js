74a608d6ed3ca2970d6e0b541b5144f1d8b52b54
Éverson Vieira de Lima <eversonvieiradelima@gmail.com>
2023-09-27 20:46:33 -0300

    Adição de banners

--- src/Components/Pesquisa/index.js
+++ src/Components/Pesquisa/index.js
@@ -2,6 +2,10 @@ import hotel_luz_azul from "../../Images/Banner/hotel_luz_azul.png";
 import passeio from "../../Images/Banner/passeio_de_barco.png";
 import pao_de_acucar from "../../Images/Banner/pao_de_acucar.png";
 import museu_dOrsay from "../../Images/Banner/museu_dOrsay.png";
+import cristo_redentor from "../../Images/Banner/cristo_redentor.png";
+import museu_do_amanha_rio from "../../Images/Banner/museu_do_amanha_rio.png";
+import mirante_dona_marta_rio from "../../Images/Banner/mirante_dona_marta_rio.png";
+import torre_eiffel from "../../Images/Banner/torre_eiffel.png";
 
 export const packagesData = [
   {
@@ -18,32 +22,44 @@ export const packagesData = [
   },
   {
     id: 3,
-    destination: "Museu D'Orsay",
+    destination: "Museu D'Orsay - Paris",
     image: museu_dOrsay,
-    description: "Descrição do pacote 3",
+    description:
+      "Instalado em uma antiga estação de trens no centro de Paris, às margens do Rio Sena, o Museu D'Orsay abriga uma vasta coleção de importantes obras de artistas como Van Gogh, Claude Monet, Auguste Rodin e Jean-Baptiste Carpeaux.",
   },
   {
     id: 4,
-    destination: "Rio de Janeiro",
+    destination: "Pão de Açúcar - Rio de Janeiro",
     image: pao_de_acucar,
-    description: "Descrição do pacote 4",
+    description:
+      "O Bondinho do Pão de Açúcar está entre os principais pontos turísticos do Rio de Janeiro e foi inaugurado em 1912, sendo o primeiro teleférico do Brasil e o terceiro no mundo. Ele liga o Morro da Urca ao Morro do Pão de Açúcar – e mais de 40 milhões de pessoas já andaram nos bondinhos.",
   },
   {
     id: 5,
-    destination: "Rio de Janeiro",
-    image: pao_de_acucar,
-    description: "Descrição do pacote 5",
+    destination: "Cristo Redentor - Rio de Janeiro",
+    image: cristo_redentor,
+    description:
+      "Literalmente de braços abertos, o Cristo Redentor, no morro do Corcovado,  recebe diariamente milhares de turistas de diversos lugares do mundo. É necessário subir 700 metros do nível do mar para, enfim, chegar ao pico onde está a estátua que é cartão postal da cidade.",
   },
   {
     id: 6,
-    destination: "Rio de Janeiro",
-    image: pao_de_acucar,
-    description: "Descrição do pacote 6",
+    destination: "Museu do Amanhã - Rio de Janeiro",
+    image: museu_do_amanha_rio,
+    description:
+      "O Museu do Amanhã, inaugurado em dezembro de 2015 no Rio de Janeiro, é um popular ponto turístico com mais de 3 milhões de visitantes. Ele se concentra em ciência, tecnologia e explora as questões de sustentabilidade e convivência que a humanidade enfrentará nas próximas décadas.",
   },
   {
     id: 7,
-    destination: "Rio de Janeiro",
-    image: pao_de_acucar,
-    description: "Descrição do pacote 7",
+    destination: "Mirante Dona Marta - Rio de Janeiro",
+    image: mirante_dona_marta_rio,
+    description:
+      "Há quem diga que é onde está a vista mais bonita do Rio de Janeiro. O acesso é feito pelo mesmo caminho que dá no Corcovado, basta seguir a placa do Mirante. O local não tem policiamento, portanto priorize visitas durante o dia, quando há mais movimento.",
+  },
+  {
+    id: 8,
+    destination: "Torre Eiffel - Paris",
+    image: torre_eiffel,
+    description:
+      "Principal símbolo de Paris, a Torre Eiffel foi planejada para o centenário da Revolução Francesa. Com cerca de 300 metros de altura, o ponto turístico oferece uma das melhores vistas de Paris durante o dia e à noite (quando acontece um espetáculo de luzes).",
   },
 ];
